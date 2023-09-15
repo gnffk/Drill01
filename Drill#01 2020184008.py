95e0498aa9bf3c9e6af4cf5f2bb89fd13df12d07
minsu <gnffk48@naver.com>
2023-09-15 16:32:05 +0900

    turtle 방향 고쳐주기

--- Drill#01 2020184008.py
+++ Drill#01 2020184008.py
@@ -1,25 +1,25 @@
 import turtle
 
-direction = 0
+
 
 def draw_w():
     turtle.stamp()
-    turtle.setheading(0)
+    turtle.setheading(90)
     turtle.forward(50)
 
 def draw_a():
     turtle.stamp()
-    turtle.setheading(-90)
+    turtle.setheading(180)
     turtle.forward(50)
 
 def draw_s():
     turtle.stamp()
-    turtle.setheading(180)
+    turtle.setheading(270)
     turtle.forward(50)
 
 def draw_d():
     turtle.stamp()
-    turtle.setheading(90)
+    turtle.setheading(0)
     turtle.forward(50)
 
 turtle.onkey(draw_w,'w')
